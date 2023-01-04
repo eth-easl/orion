2ac33fcf50af3aa46f99628b7bf09c0ca1278631
fotstrt <fotstrt95@gmail.com>
2023-01-04 17:01:39 +0000

    print kernel info

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This repo contains the implementation of our scheduler for fine-grained GPU sharing across ML workloads.
 
-### The current branch contains the implementation of CUDA/CPP backend. There are 2 main bugs that are currently being solved:
-* inconsistent results of cuda capturing mechanism
-* seg fault at multi-threaded execution
+### The current branch contains the implementation of CUDA/CPP backend. There are 3 main bugs that are currently being solved:
+* inconsistent results of cuda capturing mechanism - differences with the Nsight Nsys trace
+* seg fault at multi-threaded execution, even when printing!
+* multiple kernels when printing - why?
--- cpp_backend/cuda_capture/intercept_custom.cu
+++ cpp_backend/cuda_capture/intercept_custom.cu
@@ -1,8 +1,33 @@
 #include "intercept_custom.h"
 
+#define FUNC_DEF(func) { #func }
+
+
 using namespace std;
 
-/*cudaError_t cudaMalloc(void** devPtr, size_t size) {
+void print_kernel_invocation(int i, dim3 gridDim, dim3 blockDim) {
+	
+	printf("%d, ", i);
+	if (gridDim.y == 1 && gridDim.z == 1) {
+  		printf("--gridDim=%d ", gridDim.x);
+	} else if (gridDim.z == 1) {
+		printf("--gridDim=[%d,%d] ", gridDim.x, gridDim.y);
+	} else {
+		printf("--gridDim=[%d,%d,%d] ", gridDim.x, gridDim.y, gridDim.z);
+	}
+
+	if (blockDim.y == 1 && blockDim.z == 1) {
+		printf("--blockDim=%d ", blockDim.x);
+	} else if (blockDim.z == 1) {
+		printf("--blockDim=[%d,%d] ", blockDim.x, blockDim.y);
+	} else {
+		printf("--blockDim=[%d,%d,%d] ", blockDim.x, blockDim.y, blockDim.z);
+	}
+	//printf("\n");
+}
+
+
+cudaError_t cudaMalloc(void** devPtr, size_t size) {
 
 	printf("Caught cudaMalloc!\n");
 
@@ -12,7 +37,7 @@ using namespace std;
 	cudaError_t err = (*function)(devPtr, size);
 	return err;
 
-}*/
+}
 
 queue<kernel_record> kqueue0;
 queue<kernel_record> kqueue1;
@@ -21,24 +46,39 @@ pthread_mutex_t mutex0;
 pthread_mutex_t mutex1;
 int i=0;
 
+CUresult CUDAAPI cuGetProcAddress(const char *symbol, void **pfn, int cudaVersion, cuuint64_t flags) {
+	printf("Captured a cuGetProcAddress\n");
+}	
+
+
+CUresult cuLaunchKernel ( CUfunction f, unsigned int  gridDimX, unsigned int  gridDimY, unsigned int  gridDimZ, unsigned int  blockDimX, unsigned int  blockDimY, unsigned int  blockDimZ, unsigned int  sharedMemBytes, CUstream hStream, void** kernelParams, void** extra ) {
+
+	printf("Captured a cuLaunchKernel\n");
+
+}
+
 cudaError_t cudaLaunchKernel ( const void* func, dim3 gridDim, dim3 blockDim, void** args, size_t sharedMem, cudaStream_t stream ) {
 
 
-	printf("Captured a cudaLaunchKernel! function ptr is %p, stream is %d, gridDim is %d, blockDim is %d, sharedMem is %ld, args is %p\n", func, stream, gridDim, blockDim, sharedMem, args);
-
-	pthread_t mytid = pthread_self();
+	//printf("Captured a cudaLaunchKernel! id is %d, function ptr is %p, stream is %d, gridDim is %d, blockDim is %d, sharedMem is %ld, args is %p\n", i, func, stream, gridDim, blockDim, sharedMem, args);
 
 
 	cudaError_t (*function)(const void* func, dim3 gridDim, dim3 blockDim, void** args, size_t sharedMem, cudaStream_t stream);
 	*(void **)(&function) = dlsym (RTLD_NEXT, "cudaLaunchKernel");
 
+	//printf(", %p\n", function);
 
 	cudaError_t err = cudaSuccess;
-	if (1) { //stream != 0) {
-		printf("------------- RUN!!!!!!!!!!!!!, args is %p\n", args);
+	if (stream != 0) {
+		//printf("------------- RUN!!!!!!!!!!!!!, args is %p\n", args);
+		
+		printf("%p, ", func);
+		print_kernel_invocation(i, gridDim, blockDim);
+		printf("\n");
+
 		err = (*function)(func, gridDim, blockDim, args, sharedMem, stream);
-		printf("------------- %d, %d, %d, after func call!\n", mytid, stream, i);
-		//i+=1;
+		printf("------------- done!\n");
+		i+=1;
 		return err;
 	}
 
