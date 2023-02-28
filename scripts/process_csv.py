a5013e44c00c700c1c620c97f58b18685aa2a9f8
fotstrt <fotstrt95@gmail.com>
2023-02-28 16:26:04 +0000

    cublas fix

--- scripts/process_csv.py
+++ scripts/process_csv.py
@@ -14,6 +14,7 @@ for i, row in df.iterrows():
     x = row['Name']
     if ('memset' in x) or ('memcpy' in x):
         continue
+    #processed_kernel_names.append(x)
 
     if 'cudnn' in x:
         if 'bn_fw' in x:
@@ -21,12 +22,17 @@ for i, row in df.iterrows():
         elif ('scudnn' in x) or ('implicit_convolve_sgemm' in x):
             processed_kernel_names.append(['Conv', row['GrdX'], row['GrdY'], row['GrdZ'], row['BlkX'], row['BlkY'], row['BlkZ']])
         elif ('cudnn::winograd' in x) or ('cudnn::gemm' in x):
+            # part of cudnn mm
             pass
         else:
             processed_kernel_names.append([x,  row['GrdX'], row['GrdY'], row['GrdZ'], row['BlkX'], row['BlkY'], row['BlkZ']])
 
     elif 'volta_sgemm_128x64_nn' in x:
         processed_kernel_names.append(['Conv', row['GrdX'], row['GrdY'], row['GrdZ'], row['BlkX'], row['BlkY'], row['BlkZ']])
+
+    elif 'splitKreduce_kernel' in x:
+        # part of cublas mm
+        pass
 
     else:
         tokens = x.split('<')
@@ -35,7 +41,6 @@ for i, row in df.iterrows():
 for i,x in enumerate(processed_kernel_names):
     print(i,x)
 
-
-with open('resnet50_nofc', 'w') as f:
+with open('vgg16_bn', 'w') as f:
     for x in processed_kernel_names:
         f.write(x[0] + '\n')
