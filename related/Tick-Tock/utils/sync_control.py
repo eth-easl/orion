14b15833b23aa91569de7f295526e409e2c1238f
Xianzhe Ma <xianzma@gmail.com>
2023-03-21 10:48:39 +0100

    add more details in notification and add logging.debug to verify tick-tock

--- related/Tick-Tock/utils/sync_control.py
+++ related/Tick-Tock/utils/sync_control.py
@@ -1,5 +1,8 @@
+import logging
+
 from utils.sync_info import SyncInfo
 import torch
+import logging
 
 
 # These classes make use of the `with` pattern in Python
@@ -26,12 +29,12 @@ class ForwardControl:
         else:
             self.sync_info.eventf0.wait()
             self.sync_info.eventf0.clear()
-        # print(f"thread {self.thread_id} starts FORWARD {self.batch_idx}")
+        logging.debug(f"thread {self.thread_id} starts FORWARD {self.batch_idx}")
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
         # wait for all the submitted kernels in the stream to complete
         self.stream.synchronize()
-        # print(f"thread {self.thread_id} ends FORWARD {self.batch_idx}")
+        logging.debug(f"thread {self.thread_id} ends FORWARD {self.batch_idx}")
         if self.thread_id == 0:
             self.sync_info.eventf0.set()
         else:
@@ -62,12 +65,12 @@ class BackwardControl:
         else:
             self.sync_info.eventb0.wait()
             self.sync_info.eventb0.clear()
-        # print(f"thread {self.thread_id} starts BACKWARD {self.batch_idx}")
+        logging.debug(f"thread {self.thread_id} starts BACKWARD {self.batch_idx}")
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
         # wait for all the submitted kernels in the stream to complete
         self.stream.synchronize()
-        # print(f"thread {self.thread_id} ends BACKWARD {self.batch_idx}")
+        logging.debug(f"thread {self.thread_id} ends BACKWARD {self.batch_idx}")
         if self.thread_id == 0:
             self.sync_info.eventb0.set()
         else:
