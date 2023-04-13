371dc35b5ab0bf1e8822a8cf50d2a79fd0525ae0
Xianzhe Ma <xianzma@gmail.com>
2023-04-13 10:31:00 +0200

    fix tick-tock bug when speculating two different iters

--- related/Tick-Tock/utils/sync_info.py
+++ related/Tick-Tock/utils/sync_info.py
@@ -62,6 +62,10 @@ class TickTockSyncInfo(BasicSyncInfo):
 
     def post_measurement_prep(self, tid):
         self.no_sync_control = True
+        # the other thread might already enter next foward control
+        # before setting `no_sync_control`; set the flags to make it continue
+        self.eventf0.set()
+        self.eventf1.set()
         self.barrier.wait()
         if tid == 0:
             duration = time.time() - self.start_time
