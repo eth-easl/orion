0fffcf0d0c8afe68d7ffc2e8aa0ea38f2deb68a9
fotstrt <fotstrt95@gmail.com>
2023-04-30 15:11:35 +0000

    thread pinning

--- cpp_backend/cuda_capture/utils_interc.cu
+++ cpp_backend/cuda_capture/utils_interc.cu
@@ -9,22 +9,29 @@ int get_idx() {
 	#endif
 		//DEBUG_PRINT("-------------------my tid is %d, tids is %d, %d, %d, %d, %d \n", tid, thread_ids[0], thread_ids[1], thread_ids[2], thread_ids[3], thread_ids[4]);
 		//printf("tid is %d\n", tid);
+		int idx = -1;
 		if ((tid == thread_ids[0]) || (tid == thread_ids[3]))
-			return 0;
+			idx = 0;
 		else if ((tid == thread_ids[1]) || (tid == thread_ids[4]))
-			return 1;
+			idx = 1;
 		else if (tid == thread_ids[2])
-			return 2;
+			idx = 2;
 		else if (thread_ids[3] == 0) {
 			thread_ids[3] = tid;
-			return 0;
+			idx = 0;
 		}
 		else if (thread_ids[4] == 0) {
 			thread_ids[4] = tid;
-			return 1;
+			idx = 1;
 		}
-		else
-			return -1;
+		if (idx > -1) {
+			cpu_set_t  mask;
+			CPU_ZERO(&mask);
+			CPU_SET(idx+4, &mask);
+			int result = sched_setaffinity(0, sizeof(mask), &mask);
+			assert (result==0);
+		}
+		return idx;
 }
 
 void block(int idx, pthread_mutex_t** mutexes, queue<func_record>** kqueues) {
